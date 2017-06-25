4dc8c59b3562aef561558ecc465735070e236abb
BPrakash <bprakg@yahoo.co.in>
2017-06-25 01:31:25 -0700

    Added getters/setters and constructors

--- src/main/java/com/healthiq/simulator/bloodsugar/bean/Exercise.java
+++ src/main/java/com/healthiq/simulator/bloodsugar/bean/Exercise.java
@@ -16,5 +16,56 @@ public class Exercise {
 	private @Id @GeneratedValue long id;
 	private String name;
 	private String exerciseIndex;
+	/**
+	 * 
+	 */
+	public Exercise() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param name
+	 * @param exerciseIndex
+	 */
+	public Exercise(String name, String exerciseIndex) {
+		this.name = name;
+		this.exerciseIndex = exerciseIndex;
+	}
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the exerciseIndex
+	 */
+	public String getExerciseIndex() {
+		return exerciseIndex;
+	}
+	/**
+	 * @param exerciseIndex the exerciseIndex to set
+	 */
+	public void setExerciseIndex(String exerciseIndex) {
+		this.exerciseIndex = exerciseIndex;
+	}
 
+	
 }
