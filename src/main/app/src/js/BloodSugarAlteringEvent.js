d9cb271d630716400cfe8ea044f90bcbc6a909fe
BPrakash <bprakg@yahoo.co.in>
2017-07-16 16:54:21 -0700

    Removing auto merge errors by git

--- src/main/app/src/js/BloodSugarAlteringEvent.js
+++ src/main/app/src/js/BloodSugarAlteringEvent.js
@@ -1,44 +1,5 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-<<<<<<< HEAD
-import RowComponent from './RowComponent';
-import { connect } from "react-redux";
-import * as action from "./UserActions";
-
-
-class BloodSugarAlteringEvent extends Component {
-
-  constructor(props) {
-    super(props);
-    this.parentOnTextEntry = this.parentOnTextEntry.bind(this);
-    this.state = {
-      alteringEventList: props.store.alteringEventList
-    };
-    console.log("BloodSugarAlteringEvent - props - "+props.store.alteringEventList);
-    this.addRow = this.addRow.bind(this);
-  }
-
-  addRow(props) {
-    this.setState ({
-      alteringEventList: this.props.store.alteringEventList
-    })
-    this.props.updateAppState();
-  }
-
-  parentOnTextEntry(event) {
-    console.log(event);
-    this.props.dispatch(action.onTextEntry(event));
-  }
-
-  render() {
-    return (
-      <div className="div40pc">
-        <p> <b> Assuming day break at 7 AM </b></p>
-        <RowComponent alteringEventList={this.state.alteringEventList} 
-          masterEIOrGIList = {[]}
-          parentOnTextEntry = {this.onTextEntry}/>
-        <br/><br />
-=======
 import Row from './RowComponent';
 import { connect } from "react-redux";
 import { addRow, generateGraph, addAnEntryToEvent, showEIOrGIList } from "./UserActions";
@@ -125,7 +86,6 @@ class BloodSugarAlteringEvent extends Component {
         </div>
 
         <br /><br />
->>>>>>> stash
         <button onClick={this.addRow}>Add Row</button>
       </div>
     );
